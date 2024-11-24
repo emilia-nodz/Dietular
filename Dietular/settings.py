76a8a4b3de5491150c4e3d1978657f3b17b6e7ea
emilia-nodz <extra.maslo.game@gmail.com>
2024-11-24 15:54:50 +0100

    Proba naprawy aby dzial POST

--- Dietular/settings.py
+++ Dietular/settings.py
@@ -127,6 +127,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOWED_ORIGINS = [
+    "chrome-extension://eejfoncpjfgmeleakejdcanedmefagga",
+    "http://localhost:8000",
+]
+
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:4200',
 ]
