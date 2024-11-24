6a3430fbfdce08066dc31d9f9d484f9a9433e162
emilia-nodz <extra.maslo.game@gmail.com>
2024-11-24 18:40:39 +0100

    .gitignore zmieniony aby dzialal z angularem

--- Dietular/settings.py
+++ Dietular/settings.py
@@ -127,10 +127,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "chrome-extension://eejfoncpjfgmeleakejdcanedmefagga",
-    "http://localhost:8000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:4200',
