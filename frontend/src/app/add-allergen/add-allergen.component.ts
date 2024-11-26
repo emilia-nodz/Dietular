0b51089a26f898c7630c63bb3d1042a06108660d
emilia-nodz <extra.maslo.game@gmail.com>
2024-11-26 02:08:59 +0100

    Dodanie folderów na komponenty

---
+++ frontend/src/app/add-allergen/add-allergen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-allergen',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-allergen.component.html',
+  styleUrl: './add-allergen.component.css'
+})
+export class AddAllergenComponent {
+
+}
---
+++ frontend/src/app/allergen-list/allergen-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allergen-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './allergen-list.component.html',
+  styleUrl: './allergen-list.component.css'
+})
+export class AllergenListComponent {
+
+}
