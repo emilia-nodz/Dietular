a2e39db4b73c270849a2f98e0130a1948af758c8
wi113565 <86216@student.pb.edu.pl>
2024-12-06 11:47:30 +0100

    Mały postęp z rozdzieleniem komponentu do wyswietlenia szczegolow

--- frontend/src/app/item-details/item-details.component.ts
+++ frontend/src/app/item-details/item-details.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { ItemService } from '../services/item.service';
 import {  Item } from '../models/item.model'
+import { ItemService } from '../services/item.service';
 
 @Component({
   selector: 'app-item-details',
@@ -12,5 +12,20 @@ import {  Item } from '../models/item.model'
   styleUrl: './item-details.component.css'
 })
 export class ItemDetailsComponent {
+  items: Item[] = [];
 
+  constructor(private itemService: ItemService) {
+    this.itemService.getItems().subscribe((data: Item[]) => {
+    this.items = data;
+    });
+  }
+
+  addItem(name: string, description: string): void {
+    const newItem: Item = {name, description} as Item;
+    this.itemService.addItem(newItem).subscribe((item) => {
+    this.items.push(item);
+    })
+  }
+
+  @Input() index: number | undefined;;
 }
--- frontend/src/app/item-list/item-list.component.ts
+++ frontend/src/app/item-list/item-list.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ItemService } from '../services/item.service';
 import {  Item } from '../models/item.model'
+import { ItemDetailsComponent } from '../item-details/item-details.component';
+
 
 @Component({
   selector: 'app-item-list',
   standalone: true,
-  imports: [CommonModule,FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ItemDetailsComponent],
   templateUrl: './item-list.component.html',
   styleUrl: './item-list.component.css'
 })
 export class ItemListComponent {
-  items: Item[] = [];
+    items: Item[] = [];
 
   constructor(private itemService: ItemService) {
     this.itemService.getItems().subscribe((data: Item[]) => {
-      this.items = data;
+    this.items = data;
     });
   }
 
@@ -24,15 +26,17 @@ export class ItemListComponent {
   addItem(name: string, description: string): void {
     const newItem: Item = {name, description} as Item;
     this.itemService.addItem(newItem).subscribe((item) => {
-      this.items.push(item);
+    this.items.push(item);
     })
   }
+
+@Output() Interest: EventEmitter<number> = new EventEmitter();
 
 checker: number = 0;
 
   showDetails(x: number) {
     this.checker = x;
-
+    this.Interest.emit(x);
   }
 
 }
