f678a68b4b76ab1b2a2e7a98ce32d479d77b7f78
emilia-nodz <extra.maslo.game@gmail.com>
2024-12-09 23:23:48 +0100

    Usunięcie footera i zmiana headera

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
-import { FooterComponent } from "./layout/footer/footer.component";
 import { HeaderComponent } from "./layout/header/header.component";
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink, RouterLinkActive, CommonModule, FooterComponent, HeaderComponent, HttpClientModule, RouterModule],
+  imports: [RouterOutlet,RouterLink, RouterLinkActive, CommonModule, HeaderComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
