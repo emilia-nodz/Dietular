01b15b0c59b27807b2434140c428eb1f0472eb1d
emilia-nodz <extra.maslo.game@gmail.com>
2024-12-10 03:06:05 +0100

    Działa edycja alergenu

---
+++ frontend/src/app/edit-meal/edit-meal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-meal',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-meal.component.html',
+  styleUrl: './edit-meal.component.css'
+})
+export class EditMealComponent {
+
+}
